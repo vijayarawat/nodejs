e888b0f2c9f8287fad45ceada7a3e01343e45caf
vijayarawat <vijayarawat45@gmail.com>
2025-07-25 23:47:24 +0530

    Cretated an authentication file to add the authentication

---
+++ auth.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express;
+const db = require('./db')
+const passport = require('passport')
+const localstrategy = require('passport-local').Strategy
+
+const Person = require('./models/Person');
+const MenuItem = require('./models/MenuItem');
+
+
+passport.use(new localstrategy( async( username, password, done)=>{
+    try{
+        // console.log('Recieved credentials', username, password);
+        const user = await Person.findOne({username: username})
+        if(!user){
+            return done(null, false,{message: "Incorrect username"})
+        }
+        const isPasswordMatch = user.password === password? true:false
+        if(isPasswordMatch )
+            return done(null,user);
+        else
+            return done(null, false, {message:"incorrect password"})
+    }
+    catch(err)
+    {
+        return done(err)
+    }
+}
+))
+
+module.exports = passport
+// const  = require('passport')
